8dd8b39e208ea52e3ee045d2d4fe91c088bf4e5e
KiminonahaYo <yoshi.27.11.ry14@gmail.com>
2018-11-06 05:49:22 +0900

    Initial commit

---
+++ CommonLibrary/Error/Error.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace CommonLibrary.Error
+{
+    public delegate void OutputMethod(ExceptionInfo[] eArray);
+
+    #region 例外情報構造体（ExceptionInfo）
+    /// <summary>
+    ///     例外情報構造体
+    /// </summary>
+    public struct ExceptionInfo
+    {
+        /// <summary>例外クラス</summary>
+        private Exception e;
+
+        /// <summary>
+        ///     例外クラスプロパティ
+        /// </summary>
+        public Exception Value
+        {
+            get { return e; }
+            set { e = value; }
+        }
+
+        /// <summary>メモ（備考）</summary>
+        private string memo;
+
+        /// <summary>
+        ///     メモ（備考）プロパティ
+        /// </summary>
+        public string Memo
+        {
+            get { return memo; }
+            set { memo = value; }
+        }
+
+        /// <summary>
+        ///     例外とメモ（備考）からExceptionInfo構造体を生成します。
+        /// </summary>
+        /// <param name="e">例外クラス</param>
+        /// <param name="memo_">メモ（備考）</param>
+        /// <returns>ExceptionInfo構造体</returns>
+        public static ExceptionInfo Parse(Exception e_, string memo_)
+        {
+            ExceptionInfo eInfo;
+            eInfo.e = e_;
+            eInfo.memo = memo_;
+            return eInfo;
+        }
+    }
+    #endregion
+
+    //エラーバッファ
+    public static class ErrorBuffer
+    {
+        #region private変数
+        private static List<ExceptionInfo> eList = new List<ExceptionInfo>();
+        #endregion
+
+        #region バッファの処理
+        /// <summary>
+        ///     例外を追加する（溜める）
+        /// </summary>
+        /// <param name="e">追加する例外クラス</param>
+        public static void Add(Exception e)
+        {
+            Add(e, "");
+        }
+
+        /// <summary>
+        ///     例外を追加する（メモ（備考）追加版）
+        /// </summary>
+        /// <param name="e">例外クラス</param>
+        /// <param name="memo_">例外に関するメモ（備考）</param>
+        public static void Add(Exception e, string memo_)
+        {
+            eList.Add(ExceptionInfo.Parse(e, memo_));
+        }
+
+        /// <summary>
+        ///     例外を追加する（例外情報構造体（ExceptionInfo）指定版
+        /// </summary>
+        /// <param name="eInfo"></param>
+        public static void Add(ExceptionInfo eInfo)
+        {
+            eList.Add(eInfo);
+        }
+
+        /// <summary>
+        ///     例外をクリアする
+        /// </summary>
+        public static void Clear()
+        {
+            eList.Clear();
+        }
+        #endregion
+
+        #region 情報の取得
+        /// <summary>
+        ///     エラーバッファの指定の要素番号の例外クラスを取得する。
+        ///     静的クラスではインデクサは使用できないため、このようなメソッドを作成した。
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">引数indexで指定した要素番号が範囲外の場合発生します。</exception>
+        /// <param name="index">エラーバッファの要素番号</param>
+        /// <returns>例外情報構造体（このクラス内のExceptionInfo構造体）</returns>
+        public static ExceptionInfo Item(int index)
+        {
+            if (!(0 <= index && index < eList.Count))
+                throw new ArgumentOutOfRangeException("index", index, "引数indexで指定した要素番号が範囲外です。");
+
+            return eList[index];
+        }
+
+        /// <summary>
+        ///     foreachで回すためにコレクションで取得できる
+        /// </summary>
+        /// <returns>コレクション</returns>
+        public static IEnumerable<ExceptionInfo> GetCollection()
+        {
+            foreach (ExceptionInfo eInfo in eList)
+                yield return eInfo;
+        }
+        #endregion
+
+        #region 出力メソッド
+        /// <summary>
+        ///     例外をユーザー定義の方法で出力する
+        /// </summary>
+        /// <param name="outMethod">ユーザー定義の例外出力デリゲートメソッド</param>
+        public static void Output(OutputMethod outMethod)
+        {
+            if (eList.Count > 0)
+                outMethod(eList.ToArray());
+        }
+
+        #region 指定形式出力支援
+        #region テキストベース出力支援
+        #region private変数
+        /// <summary>
+        ///     複数出力用の文字列を生成するStringBuilder
+        ///     メソッド内でNewするとオブジェクトが増えるので、private変数とする
+        /// </summary>
+        private static StringBuilder strOut = new StringBuilder();
+
+        /// <summary>
+        ///     単数出力用の文字列を生成するStringBuilder
+        ///     メソッド内でNewするとオブジェクトが増えるので、private変数とする
+        /// </summary>
+        private static StringBuilder strOutSingle = new StringBuilder();
+        #endregion
+
+        #region メソッド
+        /// <summary>
+        ///     テキストベースで出力するための文字列を生成し、出力の支援を行います。（配列指定版）
+        ///     通常、このメソッドの引数はOutputメソッドに指定するデリゲートメソッドで取得した配列を渡します。
+        /// </summary>
+        /// <param name="eArray">例外情報構造体の配列</param>
+        /// <returns>テキストベースで出力するための文字列</returns>
+        public static string GetTextbaseString(ExceptionInfo[] eArray)
+        {
+            int iCnt = 1;
+
+            strOut.Clear();
+
+            foreach (ExceptionInfo e in eList)
+            {
+                strOut.AppendLine("●例外No." + iCnt + "：" + e.Value.GetType());
+                strOut.Append(GetTextbaseString(e));
+                iCnt++;
+            }
+
+            return strOut.ToString();
+        }
+
+        /// <summary>
+        ///     テキストベースで出力するための文字列を生成し、出力の支援を行います。（単数）
+        /// </summary>
+        /// <param name="eArray">例外情報構造体の配列</param>
+        /// <returns>テキストベースで出力するための文字列</returns>
+        public static string GetTextbaseString(ExceptionInfo e)
+        {
+            strOutSingle.Clear();
+
+            strOutSingle.AppendLine("----------------------------------------------------------------");
+            strOutSingle.AppendLine(e.Value.Message);
+            strOutSingle.AppendLine("----------------------------------------------------------------");
+            strOutSingle.AppendLine("メモ：");
+            strOutSingle.AppendLine("----------------------------------------------------------------");
+            strOutSingle.AppendLine(e.Memo);
+            strOutSingle.AppendLine("----------------------------------------------------------------");
+            strOutSingle.AppendLine("Source : " + e.Value.Source);
+            strOutSingle.AppendLine("TargetSite : " + e.Value.TargetSite);
+            strOutSingle.AppendLine("StackTrace : ");
+            strOutSingle.AppendLine("----------------------------------------------------------------");
+            strOutSingle.AppendLine(e.Value.StackTrace);
+            strOutSingle.AppendLine("----------------------------------------------------------------");
+            strOutSingle.AppendLine("InnerException : " + e.Value.InnerException);
+            strOutSingle.AppendLine("HResult : " + e.Value.HResult);
+            strOutSingle.AppendLine("HelpLink : " + e.Value.HelpLink);
+            strOutSingle.AppendLine("----------------------------------------------------------------");
+            strOutSingle.AppendLine();
+
+            return strOutSingle.ToString();
+        }
+        #endregion
+        #endregion
+        #endregion
+        #endregion
+
+        #region ※旧コード※
+        ////テキストに書き込む
+        //public static void WriteText()
+        //{
+        //    string strFilePath;
+        //    string strFileName;
+        //    string strFolderPath;
+        //    int filePathCnt;
+
+        //    //フォルダパスを取得する
+        //    strFolderPath = Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString() + @"\" +
+        //                    @"ErrorLogs\" +
+        //                    DateTime.Now.ToShortDateString() + @"\";
+        //    strFolderPath = strFolderPath.Replace("/", "");
+
+        //    strFileName = "Errors_" + DateTime.Now.ToLongTimeString() + ".log";
+        //    strFileName = strFileName.Replace(":", "");
+        //    strFilePath = strFolderPath + strFileName;
+
+        //    //フォルダを作成する
+        //    MyFolder.Create(strFolderPath);
+
+        //    //ファイルが存在したら連番で保存する
+        //    filePathCnt = 2;
+        //    while (File.Exists(strFilePath) == true)
+        //    {
+        //        strFileName = "Errors_" + DateTime.Now.ToShortTimeString() + "(" + filePathCnt + ").log";
+        //        strFileName = strFileName.Replace(":", "");
+        //        strFilePath = strFolderPath + strFileName;
+        //    }
+
+        //    //ファイルにエラーを書き込む
+        //    using (StreamWriter sw = new StreamWriter(strFilePath))
+        //    {
+        //        int iCnt = 1;
+
+        //        foreach (Exception e in eList)
+        //        {
+        //            sw.WriteLine("エラーNo." + iCnt + " " + e.ToString());
+        //            sw.WriteLine("----------------------------------------------------------------");
+        //            sw.WriteLine(e.Message);
+        //            sw.WriteLine("----------------------------------------------------------------");
+        //            sw.WriteLine("Source : " + e.Source);
+        //            sw.WriteLine("TargetSite : " + e.TargetSite);
+        //            sw.WriteLine("StackTrace : ---------------------------------------------------");
+        //            sw.WriteLine(e.StackTrace);
+        //            sw.WriteLine("----------------------------------------------------------------");
+        //            sw.WriteLine("InnerException : " + e.InnerException);
+        //            sw.WriteLine("HResult : " + e.HResult);
+        //            sw.WriteLine("HelpLink : " + e.HelpLink);
+        //            sw.WriteLine("----------------------------------------------------------------");
+        //            sw.WriteLine();
+        //            iCnt++;
+        //        }
+        //    }
+        //}
+        #endregion
+    }
+}
---
+++ CommonLibrary/Win32/IniFile.cs
@@ -0,0 +1,504 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace CommonLibrary.Win32
+{
+
+    /// <summary>
+    ///     INIファイルを管理するクラスです。
+    ///     文字列、byte配列（文字コード）、整数、実数、文字型、論理型、日付型に対応しています。
+    /// </summary>
+    /// <remarks>
+    ///     使用法例：
+    ///     変数 = (IniFileインスタンス).Keys(セクション, キー).ReadToxxx(デフォルト値);
+    ///     (IniFileインスタンス).Keys(セクション, キー).Write(値);
+    /// </remarks>
+    public class IniFile
+    {
+        #region private変数
+        /// <summary>
+        ///     INIファイルパス
+        /// </summary>
+        private string filePath;
+        #endregion
+
+        #region DLL関数
+        [DllImport("kernel32.dll")]
+        public static extern uint
+            GetPrivateProfileString(
+                string lpAppName,
+                string lpKeyName,
+                string lpDefault,
+                StringBuilder lpReturnedString,
+                uint nSize,
+                string lpFileName
+            );
+
+        [DllImport("kernel32.dll")]
+        public static extern uint
+            WritePrivateProfileString(
+                string lpAppName,
+                string lpKeyName,
+                string lpString,
+                string lpFileName
+            );
+        #endregion
+
+        #region ★IniFileSectionKeyクラス★
+        /// <summary>
+        ///     SectionとKeyの情報を持ったクラスです。
+        ///     実際にはこのクラスでINIファイルの読み書きを行います。
+        /// </summary>
+        public class IniFileSectionKey
+        {
+            #region プロパティとprivate変数
+            /// <summary>
+            ///     DLL関数に渡すセクション名
+            /// </summary>
+            private string section;
+
+            /// <summary>
+            ///     セクション名プロパティ
+            /// </summary>
+            public string Section
+            {
+                get { return this.section; }
+            }
+
+            /// <summary>
+            ///     DLL関数に渡すキー名
+            /// </summary>
+            private string key;
+
+            /// <summary>
+            ///     キー名プロパティ
+            /// </summary>
+            public string Key
+            {
+                get { return this.key; }
+            }
+
+            /// <summary>
+            ///     ファイル名
+            /// </summary>
+            private string filePath;
+
+            /// <summary>
+            ///     ファイル名プロパティ
+            /// </summary>
+            public string FilePath
+            {
+                get { return this.filePath; }
+            }
+            #endregion
+
+            #region コンストラクタ
+            /// <summary>
+            ///     コンストラクタ（セクション、キー指定）
+            /// </summary>
+            /// <param name="section_"></param>
+            /// <param name="key_"></param>
+            public IniFileSectionKey(string filePath_, string section_, string key_)
+            {
+                this.filePath = filePath_;
+                this.section = section_;
+                this.key = key_;
+            }
+            #endregion
+
+            #region 書き込み
+            #region 文字列
+            public void Write(string value)
+            {
+                WritePrivateProfileString(this.section, this.key, value, this.filePath);
+            }
+            #endregion
+
+            #region Byte配列
+            public void Write(byte[] value, Encoding enc)
+            {
+                this.Write(enc.GetString(value));
+            }
+            #endregion
+
+            #region 数値
+            #region 整数
+            public void Write(int value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(uint value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(short value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(ushort value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(long value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(ulong value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(byte value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(sbyte value)
+            {
+                this.Write(value.ToString());
+            }
+            #endregion
+            #region 実数
+            public void Write(float value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(double value)
+            {
+                this.Write(value.ToString());
+            }
+            public void Write(decimal value)
+            {
+                this.Write(value.ToString());
+            }
+            #endregion
+            #endregion
+
+            #region 文字型
+            public void Write(char value)
+            {
+                this.Write(value.ToString());
+            }
+            #endregion
+            
+            #region 論理型
+            public void Write(bool value)
+            {
+                this.Write(value.ToString());
+            }
+            #endregion
+            
+            #region 日付型
+            public void Write(DateTime value)
+            {
+                this.Write(value.ToString());
+            }
+            #endregion
+            #endregion
+
+            #region 読み込み
+            #region 文字列
+            public string ReadToString()
+            {
+                return this.ReadToString("");
+            }
+
+            public string ReadToString(string strDefault)
+            {
+                return this.ReadToString(strDefault, 256u);
+            }
+
+            public string ReadToString(string strDefault, uint bufSize)
+            {
+                StringBuilder str = new StringBuilder((int)bufSize);
+                GetPrivateProfileString(this.section, this.key, strDefault, str, bufSize, this.filePath);
+                return str.ToString();
+            }
+            #endregion
+
+            #region byte配列
+            public byte[] ReadToByteArray(Encoding enc)
+            {
+                return ReadToByteArray(enc, "");
+            }
+
+            public byte[] ReadToByteArray(Encoding enc, string strDefault)
+            {
+                return ReadToByteArray(enc, strDefault, 256u);
+            }
+
+            public byte[] ReadToByteArray(Encoding enc, string strDefault, uint bufSize)
+            {
+                string str;
+                str = this.ReadToString(strDefault, bufSize);
+                return enc.GetBytes(str.ToCharArray());
+            }
+            #endregion
+
+            #region 数値
+            #region 整数
+            #region int
+            public int ReadToInt()
+            {
+                return this.ReadToInt((int)0);
+            }
+            public int ReadToInt(int valueDefault)
+            {
+                int valueRet;
+
+                return int.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (int)0;
+            }
+            #endregion
+            #region uint
+            public uint ReadToUInt()
+            {
+                return this.ReadToUInt((uint)0);
+            }
+            public uint ReadToUInt(uint valueDefault)
+            {
+                uint valueRet;
+
+                return uint.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (uint)0;
+            }
+            #endregion
+            #region long
+            public long ReadToLong()
+            {
+                return this.ReadToLong((long)0);
+            }
+            public long ReadToLong(long valueDefault)
+            {
+                long valueRet;
+
+                return long.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (long)0;
+            }
+            #endregion
+            #region ulong
+            public ulong ReadToULong()
+            {
+                return this.ReadToULong((ulong)0);
+            }
+            public ulong ReadToULong(ulong valueDefault)
+            {
+                ulong valueRet;
+
+                return ulong.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (ulong)0;
+            }
+            #endregion
+            #region short
+            public short ReadToShort()
+            {
+                return this.ReadToShort((short)0);
+            }
+            public short ReadToShort(short valueDefault)
+            {
+                short valueRet;
+
+                return short.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (short)0;
+            }
+            #endregion
+            #region ushort
+            public ushort ReadToUShort()
+            {
+                return this.ReadToUShort((ushort)0);
+            }
+            public ushort ReadToUShort(ushort valueDefault)
+            {
+                ushort valueRet;
+
+                return ushort.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (ushort)0;
+            }
+            #endregion
+            #region byte
+            public byte ReadToByte()
+            {
+                return this.ReadToByte((byte)0);
+            }
+            public byte ReadToByte(byte valueDefault)
+            {
+                byte valueRet;
+
+                return byte.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (byte)0;
+            }
+            #endregion
+            #region sbyte
+            public sbyte ReadToSByte()
+            {
+                return this.ReadToSByte((sbyte)0);
+            }
+            public sbyte ReadToSByte(sbyte valueDefault)
+            {
+                sbyte valueRet;
+
+                return sbyte.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (sbyte)0;
+            }
+            #endregion
+            #endregion
+
+            #region 実数
+            #region float
+            public float ReadToFloat()
+            {
+                return this.ReadToFloat((float)0);
+            }
+            public float ReadToFloat(float valueDefault)
+            {
+                float valueRet;
+
+                return float.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (float)0;
+            }
+            #endregion
+            #region double
+            public double ReadToDouble()
+            {
+                return this.ReadToDouble((double)0);
+            }
+            public double ReadToDouble(double valueDefault)
+            {
+                double valueRet;
+
+                return double.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (double)0;
+            }
+            #endregion
+            #region decimal
+            public decimal ReadToDecimal()
+            {
+                return this.ReadToDecimal((decimal)0);
+            }
+            public decimal ReadToDecimal(decimal valueDefault)
+            {
+                decimal valueRet;
+
+                return decimal.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (decimal)0;
+            }
+            #endregion
+            #endregion
+            #endregion
+
+            #region 文字型
+            #region char
+            public char ReadToChar(char valueDefault)
+            {
+                char valueRet;
+
+                return char.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : (char)0;
+            }
+            #endregion
+            #endregion
+
+            #region 論理型
+            #region bool
+            public bool ReadToBool()
+            {
+                return this.ReadToBool(false);
+            }
+
+            public bool ReadToBool(bool valueDefault)
+            {
+                bool valueRet;
+
+                return bool.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : false;
+            }
+            #endregion
+            #endregion
+
+            #region 日付型
+            #region DateTime
+            public DateTime ReadToDateTime(DateTime valueDefault)
+            {
+                DateTime valueRet;
+
+                return DateTime.TryParse(this.ReadToString(valueDefault.ToString()), out valueRet) ? valueRet : new DateTime(0);
+            }
+            #endregion
+            #endregion
+            #endregion
+        }
+        #endregion
+
+        #region コンストラクタ
+        /// <summary>
+        ///     コンストラクタ（カレントディレクトリにSettings.iniを作成）
+        /// </summary>
+        public IniFile()
+        {
+            this.filePath = Directory.GetCurrentDirectory() + @"\Settings.ini";
+        }
+
+        /// <summary>
+        ///     コンストラクタ（ファイルパス指定）
+        /// </summary>
+        /// <param name="filePath_"></param>
+        public IniFile(string filePath_)
+        {
+            FileInfo fi = new FileInfo(filePath_);
+            this.filePath = fi.FullName;
+        }
+        #endregion
+
+        #region プロパティ
+        /// <summary>
+        ///     ファイルパスの設定/取得を行います。
+        /// </summary>
+        public string FilePath
+        {
+            get { return this.filePath; }
+            set
+            {
+                FileInfo fi = new FileInfo(value);
+                this.filePath = fi.FullName;
+            }
+        }
+        #endregion
+
+        #region section, keyを持ったクラスを取得（そのクラスでiniファイルを読み書きする）
+        /// <summary>
+        ///     確保済みKeyオブジェクト格納Dictionary（オブジェクトを増加させないため）
+        /// </summary>
+        private Dictionary<string, Dictionary<string, IniFileSectionKey>> diKeys = null;
+        /// <summary>
+        ///     Keyクラスを取得します。
+        ///     iniファイルの読み書きはKeyクラスで行います。
+        /// </summary>
+        /// <param name="section">セクション名</param>
+        /// <param name="key">キー名</param>
+        /// <returns>Keyクラス</returns>
+        public IniFileSectionKey Keys(string section, string key)
+        {
+            IniFileSectionKey keyRet;
+
+            //Dictionaryが確保されていない
+            if (this.diKeys == null)
+            {
+                this.diKeys = new Dictionary<string, Dictionary<string, IniFileSectionKey>>();
+            }
+
+            //sectionがDictionaryに存在しない
+            if (!this.diKeys.ContainsKey(section))
+            {
+                this.diKeys.Add(section, new Dictionary<string, IniFileSectionKey>());
+            }
+
+            //keyがDictionaryに存在しない
+            if (!this.diKeys[section].ContainsKey(key))
+            {
+                //Dictionaryに追加して、それを返却する。
+                IniFileSectionKey keyClass = new IniFileSectionKey(this.filePath, section, key);
+                this.diKeys[section].Add(key, keyClass);
+                keyRet = keyClass;
+            }
+            //section, keyがDictionaryに存在する
+            else
+            {
+                keyRet = this.diKeys[section][key];
+            }
+
+            return keyRet;
+        }
+        #endregion
+    }
+}
+
